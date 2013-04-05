3072587a8872aef5f2a21df3771a2ea7882706e7
mchlrhw <mchlrhw@users.noreply.github.com>
2013-04-05 19:09:54 +0100

    Added comments to lua script to increase user friendliness

--- colour-change.lua
+++ colour-change.lua
@@ -97,6 +97,7 @@ raspberry = Colour:new(1,   0,   0.5)
 
 --===== Custom colours =======================================================--
 -- Feel free to add your own custom colours here
+-- colour = Colour:new(red, green, blue between 0 and 1)
 dark_red  = Colour:new(0.4, 0,   0)
 
 --===== Alpha presets ========================================================--
@@ -108,6 +109,7 @@ tenth     = Alpha:new(0.1)
 
 --===== Custom alphas ========================================================--
 -- Feel free to add your own custom alphas here
+-- alpha  = Alpha:new(transparency between 0 and 1)
 
 
 --===== Global settings table ================================================--
@@ -115,22 +117,48 @@ tenth     = Alpha:new(0.1)
 settings_table = {
     {
         -- Year of the century indicator
-        name = 'smooth_time',
-        arg = '%y',
-        max = 100,
-        bg_clr_change  = true,
-        bg_clr_profile = 'default',
-        bg_alp_change  = false,
-        bg_alp_profile = tenth,
-        fg_clr_change  = true,
-        fg_clr_profile = 'default',
-        fg_alp_change  = false,
-        fg_alp_profile = half,
-        x = 180, y = 180,
-        radius = 160,
-        thickness = 4,
-        start_angle = -60,
-        end_angle = 60
+        -- This indicator's settings have been extensively commented to provide
+        -- you with a starting point for indicator customisation
+        name = 'smooth_time', ------------ The name of the conky variable used
+                                        -- for this indicator. Smooth time
+                                        -- motion in clocks is achieved by using
+                                        -- 'smooth_time', which is not a conky
+                                        -- variable, but behaves like the 'time'
+                                        -- variable for the purposes of this
+                                        -- script
+        arg = '%y', ---------------------- Any arguments that the conky variable
+                                        -- can take. 'smooth_time' takes the %y,
+                                        -- %m, %j, %d, %u, %I, %H, %M and %S
+                                        -- arguments of the 'time' variable
+        max = 100, ----------------------- The maximum value that the indicator
+                                        -- can display
+        bg_clr_change  = true, ----------- Whether to allow colour changing on
+                                        -- the background indicator
+        bg_clr_profile = 'default', ------ The colour profile for the background
+                                        -- indicator. If bg_clr_change is set to
+                                        -- false then this should be the name of
+                                        -- one of the colours defined above, eg.
+                                        -- turquoise, or white
+        bg_alp_change  = false, ---------- Whether to allow alpha changing on
+                                        -- the background indicator
+        bg_alp_profile = tenth, ---------- The alpha profile for the background
+                                        -- indicator. If bg_alp_change is set to
+                                        -- true then this should be the name of
+                                        -- one of the alpha change profiles
+                                        -- defined in the alpha_set function
+                                        -- below, eg. default, or pulse
+        fg_clr_change  = true, ----------- Same as for bg_clr_change
+        fg_clr_profile = 'default', ------ Same as for bg_clr_profile
+        fg_alp_change  = false, ---------- Same as for bg_alp_change
+        fg_alp_profile = half, ----------- Same as for bg_alp_profile
+        x = 180, y = 180, ---------------- The x and y coordinates to position
+                                        -- the indicator within the conky window
+        radius = 160, -------------------- The radius of arc/ring indicators
+        thickness = 4, ------------------- The line thickness of the indicator
+        start_angle = -60, --------------- The starting angle of arc/ring
+                                        -- indicators
+        end_angle = 60 ------------------- The ending angle of arc/ring
+                                        -- indicators
     },
     {
         -- Day of the year indicator
